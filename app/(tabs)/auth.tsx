dd5ced7760e5a8f7ad0e10da3437e353e83e19e1
marcelo799 <marcelo.silva6@mt.estudante.senai.br>
2026-03-31 19:32:53 -0400

    login

---
+++ app/(tabs)/auth.tsx
@@ -0,0 +1,54 @@
+import { View, StyleSheet} from 'react-native';
+import { Login } from "@/components/login";
+
+export default function login(){
+    return(
+        <View style={StyleSheet.container}>
+
+        </View>
+    );
+}
+
+
+Const style = StyleSheet.create
+
+
+
+
+</style> StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: 'green', 
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20,
+
+    },
+    titulo:{
+        fontFamily: 'arial',
+        marginBottom: 20,
+        fontSize: 24,
+        color: '#00000',
+        fontWeight: 'bold',
+    },
+    input:{
+        width: '100%',
+        height: 35,
+        backgroundColor: '#ffffff',
+        padding: 10,
+        borderRadius: 10,
+        marginBottom: 20,
+
+    },
+    botao:{
+        width: '100%',
+        height: 35,
+        backgroundColor: 'green',
+        padding: 10,
+        borderRadius: 10,
+    }
+
+
+})
+
+
